32fbe668fd2181ee1b326ddd0c9f066f35713f2e
Eric Rochester <erochest@virginia.edu>
2014-09-05 16:38:52 -0400

    Topical.Text.Tokenizer.parserTokenizer

--- specs/Topical/Text/TokenizerSpec.hs
+++ specs/Topical/Text/TokenizerSpec.hs
@@ -4,9 +4,15 @@
 module Topical.Text.TokenizerSpec where
 
 
-import Test.Hspec
+import           Control.Applicative
+import           Data.Attoparsec.Text
+import qualified Data.Attoparsec.Text   as A
+import           Data.Char
+import qualified Data.Text              as T
 
-import Topical.Text.Tokenizer
+import           Test.Hspec
+
+import           Topical.Text.Tokenizer
 
 
 spec :: Spec
@@ -21,3 +27,18 @@ spec = describe "Topical.Text.Tokenizer" $ do
         it "should not normalize case (or anything else)." $
             splitTokenizer "The red pony! It rides the Moors!" `shouldBe`
                 ["The", "red", "pony!", "It", "rides", "the", "Moors!"]
+
+    describe "parserTokenizer" $ do
+        it "should return all instances of a given parser." $ do
+            let space' = T.singleton <$> space
+                rWords = T.cons <$> (space *> char 'r') <*> A.takeWhile (not . isSpace)
+            parserTokenizer (string "the") "the red pony rides the moors" `shouldBe`
+                ["the", "the"]
+            parserTokenizer space' "the red pony rides the moors" `shouldBe`
+                [" ", " ", " ", " ", " "]
+            parserTokenizer space' " the red pony rides the moors " `shouldBe`
+                [" ", " ", " ", " ", " ", " ", " "]
+            parserTokenizer rWords "the red pony rides the moors" `shouldBe`
+                ["red", "rides"]
+            parserTokenizer rWords "red pony rides the moors" `shouldBe`
+                ["rides"]
--- src/Topical/Text/Tokenizer.hs
+++ src/Topical/Text/Tokenizer.hs
@@ -3,11 +3,14 @@
 
 module Topical.Text.Tokenizer
     ( splitTokenizer
+    , parserTokenizer
     ) where
 
 
-import           Data.Char          (isSpace)
-import qualified Data.Text          as T
+import           Control.Applicative
+import           Data.Attoparsec.Text
+import           Data.Char            (isSpace)
+import qualified Data.Text            as T
 
 import           Topical.Text.Types
 
@@ -15,3 +18,6 @@ import           Topical.Text.Types
 splitTokenizer :: Tokenizer
 splitTokenizer = T.split isSpace
 
+parserTokenizer :: Parser T.Text -> Tokenizer
+parserTokenizer p = either (const []) id . parseOnly (many (just p anyChar))
+    where just p' s = p <|> (s *> just p' s)
